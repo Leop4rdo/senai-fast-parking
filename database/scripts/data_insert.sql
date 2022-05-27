8fb7296ab1024d7c9865a857240a10ee9e1f007e
GABRIEL GOMES SAVIO <21276337@CFP127ALUNO.LOCAL>
2022-05-27 14:10:38 -0300

    inserting data into customer

---
+++ database/scripts/data_insert.sql
@@ -0,0 +1,30 @@
+INSERT INTO customer (
+					  name,
+                      email,
+                      phone_number,
+                      password
+					 )
+VALUES               (
+					 'John Doe',
+                     'johndoe@gmail.com',
+                     '11963677923',
+                     'senha123'
+					 ),
+                     (
+                     'Abraham Lincoln',
+                     'abrahamlikesapple@gmail.com',
+                     '53408398237',
+                     'passwordonetwothree'
+                     ),
+                     (
+                     'Leo Cemia',
+                     'email@gmail.com',
+                     '11999999999',
+                     'pkpkpkpk'
+                     ),
+                     (
+                     'Sr. Banana',
+                     'bananananica@gmail.com',
+                     '11979797979',
+                     'bananaprata'
+                     );
