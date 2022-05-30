64e18ab54fdcfd893314c68e5eacfdfd3fe93f1e
Leonardo Antunes <leonardoantunes1401@gmail.com>
2022-05-30 13:08:29 -0300

    minor changes

--- api/database_connection.php
+++ api/database_connection.php
@@ -1,9 +1,12 @@
 <?php
 
+// SERVER CONNECTION CONFIG ->
 const SERVER = "localhost";
 const USER = "root";
 const PASSWORD = "bcd127";
 const DATABASE = "db_fastparking";
+
+
 
 function connectToDatabase() {
     return new mysqli(SERVER, USER, PASSWORD, DATABASE,);
