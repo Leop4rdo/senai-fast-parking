9e49786e4f5bf37bd6df0f627aa1fd5adc4fab77
GABRIEL GOMES SAVIO <21276337@CFP127ALUNO.LOCAL>
2022-06-03 16:48:15 -0300

    adding profile activity

--- mobile/app/src/main/java/com/example/projetoestacionamento/ProfileActivity.kt
+++ mobile/app/src/main/java/com/example/projetoestacionamento/ProfileActivity.kt
@@ -1,22 +1,13 @@
 package com.example.projetoestacionamento
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageButton
 
-class DataDisplayActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_data_display)
-
-        val profileBtn = findViewById<ImageButton>(R.id.userProfile)
-
-        profileBtn.setOnClickListener{
-            val intent = Intent(this, ProfileActivity::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_profile)
 
         val previousBtn = findViewById<Button>(R.id.btnVoltar)
 
